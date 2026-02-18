6c02458661cda9e03eca136f652e1e999e3f3a70
Ajaykumarnachimuthu <ajaykumarnachimuthu37@gmail.com>
2026-02-18 16:30:35 +0530

    use case 2

---
+++ src/UseCase2PalindromeCheckerApp.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+
+public class UseCase2PalindromeCheckerApp {
+    public static void main(String[] args){
+        Scanner myObj =  new Scanner(System.in);
+        System.out.println("Input text:");
+        String inputText=myObj.nextLine();
+        inputTextMethod(inputText);
+
+
+
+
+
+
+
+    }
+    public static void inputTextMethod(String input){
+        for(int i=0;i<input.length()/2;i++){
+
+        }
+    }
+
+}
