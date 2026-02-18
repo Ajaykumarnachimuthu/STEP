3ab02c5f816e4bebbe6986013f9bd95f268a2e3b
Ajaykumarnachimuthu <ajaykumarnachimuthu37@gmail.com>
2026-02-18 15:51:58 +0530

    commit of code

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,7 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void palindrome_checker() {
+public class PalindromeChecker {
+    public static void main(String[] args){
+        displaySystemInfo();
 
-  
     }
+    static void displaySystemInfo(){
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version:1.0");
+        System.out.println("System initialized Successfully");
+
+    }
+
+
+
 }
