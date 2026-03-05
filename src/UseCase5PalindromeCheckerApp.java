c793be0427d19ffa60be3b1f80c6703760a2f09f
Ajaykumarnachimuthu <ajaykumarnachimuthu37@gmail.com>
2026-03-05 15:00:09 +0530

    uc5

---
+++ src/UseCase5PalindromeCheckerApp.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+class UseCase5PalindromeCheckerApp {
+
+
+    static void main() {
+
+
+        String input = "noon";
+
+
+        Stack<Character> stack = new Stack<>();
+
+
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+
+        boolean isPalindrome = true;
+
+
+        for (char c : input.toCharArray()) {
+
+
+            if (c != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome : " + isPalindrome);
+    }
+}
