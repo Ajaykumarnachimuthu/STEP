533c3f3d375e96b5ad1baefffdd170125894bb44
Ajaykumarnachimuthu <ajaykumarnachimuthu37@gmail.com>
2026-03-05 15:02:55 +0530

    uc7

---
+++ src/UseCase7PalindromeCheckerApp.java
@@ -0,0 +1,39 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class UseCase7PalindromeCheckerApp {
+
+
+    static void main() {
+
+
+        String input = "refer";
+
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+
+        for (char c : input.toCharArray()) {
+            deque.add(c);
+        }
+
+
+        boolean isPalindrome = true;
+
+
+        while (deque.size() > 1) {
+
+            char first = deque.removeFirst();
+            char last = deque.removeLast();
+
+            if (first != last) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome : " + isPalindrome);
+    }
+}
